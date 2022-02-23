0321709f485539bc264194d3d4be80fe4763a768
RobbnHsu <robbinhsu@gmail.com>
2022-02-23 23:05:29 +0800

    Feature: 按照書本範例補完實作

--- Course.cs
+++ Course.cs
@@ -2,6 +2,27 @@
 
 public class Course
 {
-    public string Name { get; set; }
-    public string Credits { get; set; }
+    private long _id;
+    private string _name { get; set; }
+    private int _credits { get; set; }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public void SetName(string name)
+    {
+        _name = name;
+    }
+
+    public int GetCredits()
+    {
+        return _credits;
+    }
+
+    public void SetCredits(int credits)
+    {
+        _credits = credits;
+    }
 }
