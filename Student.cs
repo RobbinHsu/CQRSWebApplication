619a29e698540d744c1ba2ac95bd68c4f2b241d2
RobbnHsu <robbinhsu@gmail.com>
2022-02-23 22:39:04 +0800

    Feature: 新增類別Student

---
+++ Student.cs
@@ -0,0 +1,18 @@
+﻿namespace CQRSWebApplication
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Enrollments Enrollments { get; set; }
+        public Disenrollments Disenrollments { get; set; }
+    }
+
+    public class Disenrollments
+    {
+    }
+
+    public class Enrollments
+    {
+    }
+}
