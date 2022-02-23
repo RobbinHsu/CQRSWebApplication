24a3c5a82c90e0cf026df52e0f239d4fb91a2388
RobbnHsu <robbinhsu@gmail.com>
2022-02-24 00:46:20 +0800

    Feature: 補完 Enrollment 的實作

---
+++ Enrollment.cs
@@ -0,0 +1,30 @@
+﻿namespace CQRSWebApplication;
+
+public class Enrollment
+{
+    private long _id;
+    private Student _student;
+    private Course _course;
+    private Grade _grade;
+
+    protected Enrollment()
+    {
+    }
+
+    public Enrollment(Student student, Course course, Grade grade)
+    {
+        _student = student;
+        _course = course;
+        _grade = grade;
+    }
+
+    public Student GetStudent()
+    {
+        return _student;
+    }
+
+    public Course GetCourse()
+    {
+        return _course;
+    }
+}
