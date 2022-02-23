3e62f8ee9c3e694e60305528f075e7ce395f4e3c
RobbnHsu <robbinhsu@gmail.com>
2022-02-24 00:47:06 +0800

    Feature: 補完 Disenrollment 的實作

---
+++ Disenrollment.cs
@@ -0,0 +1,28 @@
+﻿namespace CQRSWebApplication;
+
+public class Disenrollment
+{
+    private long id;
+    private Student _student;
+    private Course _course;
+    private DateTime _dateTime;
+    private String _comment;
+
+    public Disenrollment(Student student, Course course,
+        String comment)
+    {
+        this._student = student;
+        this._course = course;
+        this._comment = comment;
+        this._dateTime = new DateTime();
+    }
+    public Student GetStudent()
+    {
+        return _student;
+    }
+
+    public Course GetCourse()
+    {
+        return _course;
+    }
+}
